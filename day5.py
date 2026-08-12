a8173759bfe23c6dc8a191aa21674caab6ffd57e
Ken Hsiao(蕭博元) <2605851@apacer.com>
2026-08-12 13:46:06 +0800

    Day 5: if else conditional

---
+++ day5.py
@@ -0,0 +1,16 @@
+age = 16
+
+if age>=18:
+    print("你是成年人")
+else:
+    print("你是未成年人")
+
+score = 50
+
+if score =90:
+    print("優等")
+elif score >=60:
+    print("及格")
+else:
+    print("不及格")
+
